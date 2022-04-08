f58ea6b6ddbbbc04d5ed3919cd933fb7e2f94ea2
Chrystian Alvarenga <99925890+chrystian1370@users.noreply.github.com>
2022-04-08 00:30:05 +0000

    Banco de Dados

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template, request, redirect
-app = Flask('app')
 
-todos = [
-  {'title' : 'Estudar Python', 'complete':False },
-  {'title' : 'Estudar JavaScript', 'complete':True}
-]
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask('app')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+db = SQLAlchemy(app)
+
+class Todos(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(100))
+  complete = db.Column(db.Boolean)
+  category = db.Column(db.String(50))
 
 @app.route('/')
 def index():
+  todos = Todos.query.all()
   return render_template(
     'index.html',
     todos=todos
@@ -16,27 +23,37 @@ def index():
 @app.route('/create', methods=['POST'])
 def create():
   title = request.form.get('title')
-  todos.append({
-    'title':title, 
-    'complete':False})
-  
-  return redirect ('/')
+  cat = request.form.get('category')
+  new_todo = Todos(
+    title=title,
+    category=cat,
+    complete=False
+  )
+  db.session.add(new_todo)
+  db.session.commit()
+  return redirect('/')
 
 @app.route('/delete/<int:id>')
 def delete(id):
-  todos.pop(id)
+  todo = Todos.query.filter_by(id=id).first()
+  db.session.delete(todo)
+  db.session.commit()
   return redirect('/')
 
-@app.route('/complete/<int:index>')
-def complete(index):
-  todos[index]['complete'] = True
-  return redirect ('/')
+@app.route('/complete/<int:id>')
+def complete(id):
+  todo = Todos.query.filter_by(id=id).first()
+  todo.complete = True
+  db.session.commit()
+  return redirect('/')
 
-@app.route('/update/<int:index>', methods = ['POST'])
+@app.route('/update/<int:index>', methods=['POST'])
 def update(index):
   title = request.form.get('title')
   todos[index]['title'] = title
-  return redirect ('/')
-  
+  return redirect('/')
+
+# IMPORTANTE V
 if __name__ == '__main__':
+  db.create_all()
   app.run(host='0.0.0.0', port=8080)
